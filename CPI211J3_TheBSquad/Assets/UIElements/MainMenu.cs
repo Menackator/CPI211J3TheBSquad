80abdb671faba109110620ea18878fb469bb6b0f
Daniel <danieltherose@yahoo.com>
2020-03-28 19:52:10 -0700

    Added Level Design

--- CPI211J3_TheBSquad/Assets/UIElements/MainMenu.cs
+++ CPI211J3_TheBSquad/Assets/UIElements/MainMenu.cs
@@ -12,7 +12,7 @@ public class MainMenu : MonoBehaviour
     }
     public void PlayGame()
     {
-        SceneManager.LoadScene("TestScene");
+        SceneManager.LoadScene("Level");
     }
 
     public void QuitGame()
